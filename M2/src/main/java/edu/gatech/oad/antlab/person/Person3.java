455f9e0e31793dd41e334a9c6041060042bbf560
Shourya Singh <singhshourya29@gmail.com>
2017-01-25 00:33:59 -0500

    updated Person3 changes

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,6 +43,10 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+		String revInput = "";
+		for (int i = input.length() - 1; i >= 0 ; i--) {
+			revInput = revInput + input.charAt(i);
+		}
+        return revInput;
 	}
 }
