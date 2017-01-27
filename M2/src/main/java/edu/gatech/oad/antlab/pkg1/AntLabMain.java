a859ff845ae84067b311294f3e1cb4a409da2bdc
Anthony Liu <tlrnalra@gmail.com>
2017-01-27 14:40:52 -0500

    fixes missing gtid in antlabmain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Anthony Liu");
-                  toPrint += p5.toString("gburdell5");
+                  toPrint += p5.toString("aliu77");
 		  
         System.out.println(toPrint);
         
