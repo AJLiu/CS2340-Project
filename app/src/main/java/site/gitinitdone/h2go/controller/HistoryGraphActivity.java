2dd53db64d0be6ffeff7847ce4977fe617f29fb3
Shourya Singh <singhshourya29@gmail.com>
2017-03-31 20:02:22 -0400

    updated hist graph xml and activity

--- app/src/main/java/site/gitinitdone/h2go/controller/HistoryGraphActivity.java
+++ app/src/main/java/site/gitinitdone/h2go/controller/HistoryGraphActivity.java
@@ -6,8 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import java.text.DecimalFormat;
 
 import site.gitinitdone.h2go.R;
 import site.gitinitdone.h2go.model.HistoricalReportCalc;
@@ -26,7 +29,44 @@ public class HistoryGraphActivity extends AppCompatActivity {
         RadioButton virusType = (RadioButton)findViewById(R.id.historyGraphViewVirus);
         virusType.setSelected(true);
 
-        // set default of lat and long to 0.0000
+        // formatting the latitude and longitude to show (max) 6 decimal places
+        final EditText latitudeField = (EditText) findViewById(R.id.locationLat);
+        latitudeField.setText("0.0");
+        latitudeField.setText(formatLatitude(latitudeField.getText().toString()));
+
+        latitudeField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                latitudeField.setText(formatLatitude(latitudeField.getText().toString()));
+            }
+        });
+
+        final EditText longitudeField = (EditText) findViewById(R.id.locationLong);
+        longitudeField.setText("0.0");
+        longitudeField.setText(formatLongitude(longitudeField.getText().toString()));
+
+        longitudeField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                longitudeField.setText(formatLongitude(longitudeField.getText().toString()));
+            }
+        });
+    }
+
+    private String formatLongitude(String oldText) {
+        DecimalFormat formatLong = new DecimalFormat("##0.000000");
+        if (oldText.trim().isEmpty()) {
+            oldText = "0";
+        }
+        return formatLong.format(Double.parseDouble(oldText.trim()));
+    }
+
+    private String formatLatitude(String oldText) {
+        DecimalFormat formatLat = new DecimalFormat("#0.000000");
+        if (oldText.trim().isEmpty()) {
+            oldText = "0";
+        }
+        return formatLat.format(Double.parseDouble(oldText.trim()));
     }
 
     public static void finishedDataCalc() {
