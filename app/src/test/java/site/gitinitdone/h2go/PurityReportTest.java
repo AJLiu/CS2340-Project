388b43ece7d71b62585521ddd85b03c76ab5dfc5
Suraj Masand <smasand6@gatech.edu>
2017-04-05 22:18:59 -0400

    Started PurityReport JUnit tests

---
+++ app/src/test/java/site/gitinitdone/h2go/PurityReportTest.java
@@ -0,0 +1,18 @@
+package site.gitinitdone.h2go;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import site.gitinitdone.h2go.model.PurityReport;
+
+import java.util.Date;
+
+/**
+ * Created by Suraj Masand on 4/5/2017
+ * JUnit tests for the Purity Report Class
+ * Specifically, this will test the getReportStringFormatter method
+ */
+public class PurityReportTest {
+
+}
