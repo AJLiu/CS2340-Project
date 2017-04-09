c7a8cc24a2751b6b4fbd966d47432a31a2fa8413
avantij <avantij14@gmail.com>
2017-04-09 19:46:49 -0400

    updated getAveragejunit tests

--- app/src/test/java/site/gitinitdone/h2go/HistoricalReportAverageTest.java
+++ app/src/test/java/site/gitinitdone/h2go/HistoricalReportAverageTest.java
@@ -25,20 +25,22 @@ public class HistoricalReportAverageTest {
     List<PurityReport> testList;
     HistoricalReportCalc historicalReportCalc1;
     HistoricalReportCalc historicalReportCalc2;
-
+    HistoricalReportCalc historicalReportCalc3;
+    private final long offsetTime = Long.parseLong("40150624214"); // creates offset by 1 year
 
     @Before
     public void setup() {
 
         test1 = new PurityReport(82.0, 90.0, 1, "Avanti", new Date().getTime(), PurityReport.OverallCondition.UNSAFE, 100, 25); 
         test2 = new PurityReport(82.0, 90.0, 2, "Vinny", new Date().getTime(), PurityReport.OverallCondition.TREATABLE, 300, 75);
-        test3 = new PurityReport(82.0, 90.0, 2, "Shreya", new Date(2017, 1, 16), PurityReport.OverallCondition.TREATABLE, 100, 200);
+        test3 = new PurityReport(82.0, 90.0, 2, "Shreya", new Date().getTime() - offsetTime, PurityReport.OverallCondition.TREATABLE, 20, 35);
         testList = new ArrayList<>(); 
-        testList[0] = test1;
-        testList[1] = test2; 
-        testList[2] = test3; 
+        testList.add(test1);
+        testList.add(test2);
+        testList.add(test3);
         historicalReportCalc1 = new HistoricalReportCalc(82.0, 90.0, 2017, "Virus");
-        historicalReportCalc2 = new HistoricalReportCalc(82.0, 90.0, 2017, "Contaminant")
+        historicalReportCalc2 = new HistoricalReportCalc(82.0, 90.0, 2017, "Contaminant");
+        historicalReportCalc3 = new HistoricalReportCalc(82.0, 90.0, 2016, "Virus");
 
 
     }
@@ -47,15 +49,34 @@ public class HistoricalReportAverageTest {
     @Test
     public void checkAvgTest() {
         double[] result1 = historicalReportCalc1.getAverages(testList); 
-        double[] result2 = historicalReportCalc2.getAverages(testList); 
+        double[] result2 = historicalReportCalc2.getAverages(testList);
+        double[] result3 = historicalReportCalc3.getAverages(testList);
         assertEquals(12, result1.length); 
-        assertEquals(12, result2.length); 
-        assertEquals(result1[3], 200);
-        assertEquals(result2[3], 50);
-        assertEquals(result1[0], 100);
-        assertEquals(result2[0], 200);
-        assertEquals(result1[4], 0);
-        assertEquals(result2[4], 0); 
+        assertEquals(12, result2.length);
+        assertEquals(12, result3.length);
+
+        //checking to see if Jan entry is 0
+        assertEquals(result3[0], 20, 0);
+
+        assertEquals(result1[3], 200, 0);
+        assertEquals(result2[3], 50, 0);
+
+        assertEquals(result1[4], 0, 0);
+        assertEquals(result2[4], 0, 0);
+        assertEquals(result1[5], 0, 0);
+        assertEquals(result2[5], 0, 0);
+        assertEquals(result1[6], 0, 0);
+        assertEquals(result2[6], 0, 0);
+        assertEquals(result1[7], 0, 0);
+        assertEquals(result2[7], 0, 0);
+        assertEquals(result1[8], 0, 0);
+        assertEquals(result2[8], 0, 0);
+        assertEquals(result1[9], 0, 0);
+        assertEquals(result2[9], 0, 0);
+        assertEquals(result1[10], 0, 0);
+        assertEquals(result2[10], 0, 0);
+        assertEquals(result1[11], 0, 0);
+        assertEquals(result2[11], 0, 0);
     }
 
 }
