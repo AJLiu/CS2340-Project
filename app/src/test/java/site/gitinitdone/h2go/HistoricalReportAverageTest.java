74a35961a39c5eb6f8cb1cf8c6fab7d25daeace3
smagesh3 <shreya.mnms@gmail.com>
2017-04-09 17:26:45 -0400

    HistoricalReportCalcAverages test almost fixed

--- app/src/test/java/site/gitinitdone/h2go/HistoricalReportAverageTest.java
+++ app/src/test/java/site/gitinitdone/h2go/HistoricalReportAverageTest.java
@@ -5,9 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
+import site.gitinitdone.h2go.model.HistoricalReportCalc;
 import site.gitinitdone.h2go.model.PurityReport;
 import site.gitinitdone.h2go.model.PurityReport.OverallCondition;
 
@@ -18,24 +20,25 @@ import site.gitinitdone.h2go.model.PurityReport.OverallCondition;
 public class HistoricalReportAverageTest {
 
     PurityReport test1;
-    PurityReport test2; 
-    List<PurityReport> testList; 
-    HistoricalReportCal historicalReportCalc1; 
-    HistoricalReportCal historicalReportCalc2; 
+    PurityReport test2;
+    PurityReport test3;
+    List<PurityReport> testList;
+    HistoricalReportCalc historicalReportCalc1;
+    HistoricalReportCalc historicalReportCalc2;
 
 
     @Before
     public void setup() {
 
         test1 = new PurityReport(82.0, 90.0, 1, "Avanti", new Date().getTime(), PurityReport.OverallCondition.UNSAFE, 100, 25); 
-        test2 = new PurityReport(82.0, 90.0, 2, "Vinny", new Date().getTime(), PurityReport.OverallCondition.TREATEABLE, 300, 75);
-        test3 = new PurityReport(82.0, 90.0, 2, "Shreya", new Date(2017, 1, 16), PurityReport.OverallCondition.TREATEABLE, 100, 200);
+        test2 = new PurityReport(82.0, 90.0, 2, "Vinny", new Date().getTime(), PurityReport.OverallCondition.TREATABLE, 300, 75);
+        test3 = new PurityReport(82.0, 90.0, 2, "Shreya", new Date(2017, 1, 16), PurityReport.OverallCondition.TREATABLE, 100, 200);
         testList = new ArrayList<>(); 
         testList[0] = test1;
         testList[1] = test2; 
         testList[2] = test3; 
-        historicalReportCalc1 = new HistoricalReportCal(82.0, 90.0, 2017, "Virus");
-        historicalReportCalc2 = new HistoricalReportCal(82.0, 90.0, 2017, "Contaminant")
+        historicalReportCalc1 = new HistoricalReportCalc(82.0, 90.0, 2017, "Virus");
+        historicalReportCalc2 = new HistoricalReportCalc(82.0, 90.0, 2017, "Contaminant")
 
 
     }
